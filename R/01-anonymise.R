60469f8e169779e2a6e2433f4ffb5607ad49bc79
ppintosilva <ppintodasilva@gmail.com>
2021-03-25 17:12:29 +0000

    Anonymise raw data

---
+++ R/01-anonymise.R
@@ -0,0 +1,61 @@
+col_names <- c(
+  # cover
+  "t",
+  "interviewee",
+  "organisation",
+  "sector",
+  "size",
+  # agility
+  "storage",
+  "metadata",
+  "interop_model",
+  "interop_fields",
+  "interop_docs",
+  "interop_reusable",
+  "skills_io",
+  "skills_transf",
+  "skills_devops",
+  "testing",
+  "readiness",
+  "agility_notes",
+  # intelligence
+  "methodology",
+  "vailidity",
+  "tools",
+  "doc_problem",
+  "doc_errors",
+  "doc_decisions",
+  "doc_results",
+  "skills_cleaning",
+  "skills_exploratory",
+  "skills_modelling",
+  "skills_ml",
+  "skills_interpretation",
+  "skills_visuals",
+  "int_notes"
+)
+
+raw_data <- read_csv(
+  file = "data/00-raw_data.csv",
+  col_names = col_names,
+  col_types = cols(
+    .default = col_character()
+  ),
+  skip = 1
+)
+
+anonymisation_table <-
+  read_csv(
+    file = "data/00-anonymisation-table.csv",
+    col_types = cols(
+      .default = col_character()
+    )
+  )
+
+raw_data_anonymised <-
+  raw_data %>%
+  inner_join(anonymisation_table) %>%
+  select(-c(t,interviewee,organisation,sector,size)) %>%
+  select(id, everything())
+
+write_rds(raw_data_anonymised, "data/01-raw-data-anonymised.rds")
